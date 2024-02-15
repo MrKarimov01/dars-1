cefa084593bb0431bf93d2f624b916d86862bf38
MrKarimov01 <108998528+MrKarimov01@users.noreply.github.com>
2024-02-15 21:22:13 +0500

    birinchi kode

---
+++ mian.js
@@ -0,0 +1,34 @@
+// let oylar = "mart";
+
+// switch (oylar) {
+//     case "yanvar":
+//     case "fevral":
+//         console.log("31");
+//         break;
+//     case "mart":
+//         console.log("30");  
+//         break;  
+//     default:
+//         console.log("salom");
+//         break;
+// }
+
+// name()
+
+// function name(name) {
+//     console.log("name");
+// }
+
+// // 1 + 2 = 5
+
+
+
+
+// const arrow = ( ) =>{
+//     console.log("arrow");
+// }
+
+
+
+
+
